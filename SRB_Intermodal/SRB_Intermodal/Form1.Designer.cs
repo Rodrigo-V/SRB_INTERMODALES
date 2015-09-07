d898d06479ea783d63be189ef174e8f64db7ba26
Rodrigo-V <rvilchezf@outlook.com>
2015-09-07 05:27:44 -0300

    menu  contextual

--- SRB_Intermodal/SRB_Intermodal/Form1.Designer.cs
+++ SRB_Intermodal/SRB_Intermodal/Form1.Designer.cs
@@ -28,10 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.radTextBoxControl1 = new Telerik.WinControls.UI.RadTextBoxControl();
             this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.habilitarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dehabilitarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.radTextBoxControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // radTextBoxControl1
@@ -50,6 +55,28 @@
             this.radButton1.Text = "radButton1";
             this.radButton1.ThemeName = "Office2013Light";
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.habilitarToolStripMenuItem,
+            this.dehabilitarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 48);
+            this.contextMenuStrip1.Text = "Iniciar";
+            // 
+            // habilitarToolStripMenuItem
+            // 
+            this.habilitarToolStripMenuItem.Name = "habilitarToolStripMenuItem";
+            this.habilitarToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.habilitarToolStripMenuItem.Text = "Habilitar";
+            // 
+            // dehabilitarToolStripMenuItem
+            // 
+            this.dehabilitarToolStripMenuItem.Name = "dehabilitarToolStripMenuItem";
+            this.dehabilitarToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.dehabilitarToolStripMenuItem.Text = "Deshabilitar";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -59,8 +86,10 @@
             this.Controls.Add(this.radTextBoxControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.radTextBoxControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -69,6 +98,9 @@
 
         private Telerik.WinControls.UI.RadTextBoxControl radTextBoxControl1;
         private Telerik.WinControls.UI.RadButton radButton1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem habilitarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dehabilitarToolStripMenuItem;
     }
 }
 
